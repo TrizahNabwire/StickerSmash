338aa7871b0a409e1a7ab2dff912431e64d3aeeb
unknown <trizahnabwire20@gmail.com>
2025-07-10 02:07:24 +0300

    Use domtoimage instead of htmlcanvas for screenshot and save on web

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
-import { View, StyleSheet, ImageSourcePropType } from "react-native";
+import { View, StyleSheet, ImageSourcePropType, Platform} from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import { useState, useRef, useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from "react-native-view-shot";
 
-import { Platform } from "react-native";
-import html2canvas from "html2canvas";
+// import html2canvas from "html2canvas";
+import domtoimage from 'dom-to-image';
 
 import ImageViewer from '@/components/ImageViewer';
 import Button from "@/components/Button";
@@ -68,41 +68,81 @@ export default function Index() {
     setIsModalVisible(false);
   };
 
+  // const onSaveImageAsync = async () => {
+  //   try {
+  //     if (Platform.OS === 'web') {
+  //       // @ts-ignore
+  //       const element = imageRef.current?.containerRef?.current || imageRef.current;
+  //       if (!element) return;
+
+  //       const canvas = await html2canvas(element as HTMLElement);
+  //       const dataUrl = canvas.toDataURL('image/png');
+
+  //       const link = document.createElement('a');
+  //       link.href = dataUrl;
+  //       link.download = 'sticker-smash-screenshot.png';
+  //       link.click();
+  //       alert('Image Saved!');
+  //     }else{
+  //     const localUri = await captureRef(imageRef, {
+  //       height: 440,
+  //       quality: 1,
+  //     });
+
+  //     await MediaLibrary.saveToLibraryAsync(localUri);
+  //     if (localUri) {
+  //       alert('Screenshot Saved!');
+  //     }
+  //   }
+  //   } catch (e) {
+  //     console.log(e);
+  //     alert('Failed to save the image. Please try again.');
+  //   }
+
+  // };
+
+  // const [status, requestPermission] = MediaLibrary.usePermissions();
+
   const onSaveImageAsync = async () => {
-    try {
-      if (Platform.OS === 'web') {
-        // @ts-ignore
-        const element = imageRef.current?.containerRef?.current || imageRef.current;
-        if (!element) return;
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-        const canvas = await html2canvas(element as HTMLElement);
-        const dataUrl = canvas.toDataURL('image/png');
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Screenshot Saved!');
+        }
+      } catch (e) {
+        console.log(e);
+        alert('Failed to save the image. Please try again.');
+      }
+    }
+    else {
+      try {
+        if (!imageRef.current) {
+          alert('No image to save.');
+          return;
+        }
+        const dataUrl = await domtoimage.toJpeg(imageRef.current as unknown as Node, {
+          width: 320,
+          height: 440,
+          quality: 0.95,
+        });
 
-        const link = document.createElement('a');
+        let link = document.createElement('a');
         link.href = dataUrl;
         link.download = 'sticker-smash-screenshot.png';
         link.click();
         alert('Image Saved!');
-      }else{
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
-
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert('Screenshot Saved!');
+      } catch (e) {
+        console.log(e);
+        alert('Failed to save the image. Please try again.');
       }
     }
-    } catch (e) {
-      console.log(e);
-      alert('Failed to save the image. Please try again.');
-    }
-
   };
-
-  // const [status, requestPermission] = MediaLibrary.usePermissions();
-
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
@@ -164,4 +204,3 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
 })
-
